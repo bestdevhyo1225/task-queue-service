feab91ba024b0f22a879445a42ace2a2497938ab
janghyoseog <bestdevhyo1225@gmail.com>
2019-09-20 16:52:20 +0900

    feat: SQS Url 추가

--- config/secret.ts
+++ config/secret.ts
@@ -5,7 +5,9 @@ if (fs.existsSync('.env')) {
     dotenv.config({ path : '.env' });
 }
 
+export const TASK_QUEUE_SQS_URL = process.env.TASK_QUEUE_SQS_URL as string;
+
 export const secret = () => ({
-    TASK_QUEUE_ROLE : process.env.TASK_QUEUE_ROLE,
-    REGION : process.env.REGION
+    TASK_QUEUE_ROLE : process.env.TASK_QUEUE_ROLE as string,
+    REGION : process.env.REGION as string
 });
